4f9ae734fdbeef9d9eda71193937b7c67a708d05
deepak mehra <deepak7mhera7@gmail.com>
2024-01-31 11:48:32 +0530

    complete porfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,80 @@
+import { useEffect, useRef, useState } from "react";
 import Card from "./components/Card"
 import IntroSection from "./components/IntroSection"
 import Navbar from "./components/Navbar"
 import SmallHeading from "./components/SmallHeading"
+import FindMe from "./components/FindMe";
+import Footer from "./components/Footer";
 
 
 function App() {
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver(
+      ([entry])=>{
+        setIsIntersecting(entry.isIntersecting);
+      },{
+        rootMargin: "-5px"
+      }
+    );
+    
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  },[isIntersecting])
+
+  useEffect(() => {
+    if (isIntersecting && ref.current) {
+      ref.current.classList.add("show")
+    }else{
+      ref.current?.classList.remove("show")
+    }
+  }, [isIntersecting]);
+  
+
+
+  
+
+
   return (
     <>
 
     <Navbar/>
-    <main className="mt-20 max-w-[80ch] mx-auto p-4 md:p-0 ">
-      <IntroSection/>
+    <main className="mt-20 max-w-[80ch] mx-auto p-4 md:p-0">
+      <div className="min-h-[400px] flex items-center">
+
+        <IntroSection/>
+      </div>
 
 
       <div className="mt-20">
 
       <SmallHeading text={"Introduction"}/>
 
-      <div className="mt-5 flex flex-col gap-5">
-        <li>I am btech 3rd year  student </li>
-        <li>I am a competetive programmer</li>
+      <div ref={ref} className="mt-5 flex flex-col gap-5 text-xl hide min-h-80">
+        <li>I am 3rd year student in Dronacharya College of Engineering </li>
+        <li>I love problem solving and competitive programming.</li>
         <li>I am machine learning enthusiactic </li>
+        <li>My field of Interest's are building new  Web Technologies</li>
       </div>
 
       </div>
 
        <div className="mt-20">
         <SmallHeading text="Projects"/>
-        <div className="mt-20 flex gap-3 overflow-x-auto mb-4">
+        <div className={`mt-20 flex gap-3 md:gap-4 overflow-x-auto mb-4`}>
           <div className="min-w-[328px]">
           <Card/>
 
           </div>
-          <div className="min-w-[328px]">
+          <div className="min-w-[328px] ">
           <Card/>
 
           </div>
@@ -47,7 +89,16 @@ function App() {
        </div>
 
 
+       <div className="mt-[12rem]  flex justify-center">
+        <FindMe/>
+
+       </div>
     </main>
+
+    <footer className="mt-20">
+      <Footer/>
+
+    </footer>
 
   
     
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./Card.module.css"
 
 export default function Card() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver(
+      ([entry])=>{
+        setIsIntersecting(entry.isIntersecting);
+      },{
+        rootMargin: "-5px"
+      }
+    );
+    
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  },[isIntersecting])
+
+  useEffect(() => {
+    if (isIntersecting && ref.current) {
+      ref.current.classList.add(`${styles.show}`)
+    } /* else if (ref.current) {
+      ref.current.classList.remove(`${styles.show}`)
+    } */
+  }, [isIntersecting]);
+
+
   return (
-    <div className="bg-rose-500 min-h-[400px] max-w-[328px] rounded-lg">
-        helo world
+    <div ref={ref} className={` ${styles.hidden}  ${styles.animate} max-w-[328px]  min-h-[400px] relative rounded-2xl `}>
+        <div className={` bg-purple-500 shadow-4xl ${styles.card__side} ${styles.card__sideFront} absolute w-full h-full rounded-2xl p-4 `}>
+          this is another content
+        </div>
+         <div className={`  rounded-lg shadow-xl ${styles.card__side} ${styles.card__sideBack} absolute h-full w-full rounded-2xl p-4`}>
+          this is content
+        </div>
 
       
     </div>
